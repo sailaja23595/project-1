d3e125028e1f0718c8a5029129afdb0f30893770
sailaja23595 <arigeli.sailaja@msitprogram.net>
2020-04-16 12:16:20 +0530

    Handled server side request with flask using POST and GET

--- project1/Index.py
+++ project1/Index.py
@@ -1,13 +1,16 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Project 1: TODO"
 
-@app.route("/Register")
-def Register():
-    return render_template("Registration.html")
-
+@app.route("/Register", methods = ['POST', 'GET'])
+def Output():
+    if request.method =='POST':
+        result = request.form
+        return render_template("Output.html",Output = result)
+    else:
+        return render_template("Registration.html")
 if __name__ == '__main__':
     app.run(debug = True)
